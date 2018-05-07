09bb4b6b8ae94aea3ae3941f608091dd444348a8
Qarchli Mouad <31633271+qarchli@users.noreply.github.com>
2018-05-07 17:57:32 +0100

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -2,8 +2,12 @@ import java.math.BigInteger;
 import java.util.Random;
 
 public class Prime {
-
-	public static BigInteger getPrime( int minVal, int maxVal, int certainty ) {
+	
+	/*
+	 *method charged with the generation of a random integer within the range [minVal,maxVal] 
+	 *and testing whether it is a probable prime with a given certainty
+	 */
+	public static BigInteger generatePrime( int minVal, int maxVal, int certainty ) {
 
 		BigInteger max = new BigInteger( Integer.toString( maxVal ) );
 		BigInteger min = new BigInteger( Integer.toString( minVal ) );
